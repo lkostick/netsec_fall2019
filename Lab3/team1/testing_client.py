0b3ca47d903ad35b38b955ae6bed77d9b5b159b0
Sepehr Abdous <sepehrabdous1375@gmail.com>
2019-12-06 12:28:31 -0500

    automate tester

--- Lab3/team1/testing_client.py
+++ Lab3/team1/testing_client.py
@@ -1,3 +1,6 @@
+import datetime
+import time
+
 from CipherUtil import loadCertFromFile
 from OnlineBank import BankClientProtocol, OnlineBankConfig
 
@@ -14,7 +17,7 @@ STACK = "crap"
 
 
 class GameClientProtocol(asyncio.Protocol):
-    def __init__(self, loop, bank_client, bank_addr, bank_port, username, user_acct, commands=None):
+    def __init__(self, loop, bank_client, bank_addr, bank_port, username, user_acct, commands=None, team_number=None):
         self.loop = loop
         self.bank_client = bank_client
         self.bank_addr = bank_addr
@@ -24,6 +27,11 @@ class GameClientProtocol(asyncio.Protocol):
         self.transferResult = None
         self.game_status = None
         self.commands = commands
+        self.flyingkeyHit = False
+        self.flyingringHit = True
+        self.team_number = team_number
+        if self.team_number == '3':
+            self.flyingringHit = False
 
         self.loop.add_reader(sys.stdin, self.game_next_input)
         self.deserializer = PacketType.Deserializer()
@@ -35,6 +43,20 @@ class GameClientProtocol(asyncio.Protocol):
 
         p = create_game_init_packet(self.username)
         self.transport.write(p.__serialize__())
+
+    def send_next_command(self):
+        time.sleep(1)
+        if "hit flyingkey" in self.commands[0] and not self.flyingkeyHit:
+            self.write(self.commands[0])
+        elif "hit flyingring" in self.commands[0] and not self.flyingringHit:
+            self.write(self.commands[0])
+        else:
+            if "hit flyingkey" in self.commands[0] and self.flyingkeyHit:
+                self.commands.pop(0)
+            if "hit flyingring" in self.commands[0] and self.flyingringHit:
+                self.commands.pop(0)
+            command = self.commands.pop(0)
+            self.write(command)
 
     def data_received(self, data):
         print('Something received from {}: {}'.format(self.transport.get_extra_info('peername'), data))
@@ -54,8 +76,14 @@ class GameClientProtocol(asyncio.Protocol):
                 print('Packet Info: GameRequirePayPacket\n'
                       'reponse: {}\n'
                       'status: {}\n'.format(game_response, self.game_status))
+                if "flyingkey" in game_response and "falls" in game_response:
+                    self.flyingkeyHit = True
+                if "flyingring" in game_response and "falls" in game_response:
+                    self.flyingringHit = True
                 game_over = self.game_status == 'escaped' or self.game_status == 'dead'
                 print(game_response)
+                if self.commands is not None:
+                    self.send_next_command()
 
     def write(self, msg):
         p = create_game_command(msg)
@@ -110,9 +138,6 @@ class GameClientProtocol(asyncio.Protocol):
         if self.commands is None:
             game_input = sys.stdin.readline().strip()
             self.write(game_input)
-        else:
-            command = self.commands.pop(0)
-            self.write(command)
 
     def flush_output(self, *args, **kargs):
         print(*args, **kargs)
@@ -130,12 +155,108 @@ def main(args):
     EnablePresetLogging(PRESET_VERBOSE)
 
     team_list = []
-    team_list.append(Team(team_number='2', host='20194.2.57.98', port=2222))
-    team_list.append(Team(team_number='3', host='20194.3.6.9', port=333))
+
+    '''
+    commands to win team to:
+    1) "enter code to the coded lock"
+    2) enter current time on your laptop
+    3) "open chest"
+    4) "get hammer from chest"
+    5) "hit flyingkey with hammer" (do it till you hit it)
+    6) "get key"
+    7) "unlock door with key"
+    8) "open door"
+    '''
+    commands = ["enter code to the coded lock", datetime.datetime.now().strftime("%H%M"), "open chest", "get hammer from chest", "hit flyingkey with hammer",
+                "get key", "unlock door with key", "open door"]
+    team_list.append(Team(team_number='2', host='20194.2.57.98', port=2222, commands=commands))
+
+    '''
+    commands to win team to:
+    1) "look mirror"
+    2) "look puzzle"
+    3) "answer 368.2"
+    4) "look mirror"
+    5) "get hairpin"
+    6) "unlock chest with hairpin"
+    7) "open chest"
+    8) "get hammer from chest"
+    9) "get spear from chest"
+    10) "hit flyingkey with hammer" (do it till you hit it)
+    11) "hit flyingring with spear" (do it till you hit it)
+    12) "get flyingkey"
+    13) "get flyingring"
+    14) "get glue"
+    15) "combine flyingkey with flyingring using glue"
+    16) "get key"
+    17) "unlock door with key"
+    18) "hit monster with hammer"
+    19) "open door"
+    
+    '''
+    commands = ["look mirror", "look puzzle", "answer 368.2", "look mirror",
+                "get hairpin", "unlock chest with hairpin", "open chest",
+                "get hammer from chest", "get spear from chest", "hit flyingkey with hammer",
+                "hit flyingring with spear", "get flyingkey", "get flyingring",
+                "get glue", "combine flyingkey with flyingring using glue", "get key",
+                "unlock door with key", "hit monster with hammer", "open door"]
+    team_list.append(Team(team_number='3', host='20194.3.6.9', port=333, commands=commands))
+
+    '''
+    commands to win team to:
+    1) "look mirror"
+    2) 
+
+    '''
     team_list.append(Team(team_number='4', host='20194.4.4.4', port=8666))
-    team_list.append(Team(team_number='5', host='20194.5.20.30', port=8989))
-    team_list.append(Team(team_number='6', host='20194.6.20.30', port=16666))
-    team_list.append(Team(team_number='9', host='20194.9.1.1', port=7826))
+
+    '''
+    commands to win team to:
+    1) "look mirror"
+    2) "get hairpin"
+    3) "unlock chest with hairpin"
+    4) "open chest"
+    5) "get hammer from chest"
+    6) "hit flyingkey with hammer" (do it till you hit it)
+    7) "get key"
+    8) "unlock door with key"
+    9) "open door"
+
+    '''
+    commands = ["look mirror", "get hairpin", "unlock chest with hairpin", "open chest",
+                "get hammer from chest", "hit flyingkey with hammer", "get key",
+                "unlock door with key", "open door"]
+    team_list.append(Team(team_number='5', host='20194.5.20.30', port=8989, commands=commands))
+
+    '''
+    commands to win team to:
+    1) "hit flyingkey with hammer" (do it till you hit it)
+    2) "get key"
+    3) "unlock door with key"
+    4) "open door"
+
+    '''
+    commands = ["hit flyingkey with hammer", "get key", "unlock door with key", "open door"]
+    team_list.append(Team(team_number='6', host='20194.6.20.30', port=16666, commands=commands))
+
+    '''
+    commands to win team to:
+    0) "get shield"
+    1) "look mirror"
+    2) "get hairpin"
+    3) "unlock chest with hairpin"
+    4) "open chest"
+    5) "get hammer from chest"
+    6) "hit flyingkey with hammer" (do it till you hit it)
+    7) "get key"
+    8) "unlock door with key"
+    9) "open door"
+
+    '''
+    commands = ["get shield", "look mirror", "get hairpin",
+                "unlock chest with hairpin", "open chest", "get hammer from chest",
+                "hit flyingkey with hammer", "get key", "unlock door with key", "open door"]
+    team_list.append(Team(team_number='9', host='20194.9.1.1', port=7826, commands=commands))
 
     while True:
         team_number = input("Enter team number escaperoom you want to play: ")
@@ -164,7 +285,7 @@ def main(args):
     loop = asyncio.get_event_loop()
 
     coro = playground.create_connection(
-        lambda: GameClientProtocol(loop, bank_client, bank_addr, bank_port, username, user_acct, commands=commands), host=host, port=port, family=STACK)
+        lambda: GameClientProtocol(loop, bank_client, bank_addr, bank_port, username, user_acct, commands=commands, team_number=team_number), host=host, port=port, family=STACK)
     client = loop.run_until_complete(coro)
 
     try:
